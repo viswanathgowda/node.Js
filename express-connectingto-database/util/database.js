e2eab46cd29ff99ab9243db32b1e5369356d3be6
viswanath <viswavishu40.com>
2024-02-23 09:49:33 +0530

    installing the sequelize using 'npm install --save sequelize' and connecting to database. implemented the database model.

--- express-connectingto-database/util/database.js
+++ express-connectingto-database/util/database.js
@@ -1,10 +1,5 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize')
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Vishwa@123'
-})
+const sequelize = new Sequelize('node-complete', 'root', 'Vishwa@123', {dialect: 'mysql', host: 'localhost'})
 
-module.exports = pool.promise()
+module.exports = sequelize
